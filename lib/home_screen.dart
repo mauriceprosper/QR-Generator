b0bccca5cb177824763715e93359ef81633d62be
mauriceprosper1 <mauriceprosper1@gmail.com>
2024-08-21 14:58:11 +0000

    QR Code Generator in Flutter

---
+++ lib/home_screen.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconly/iconly.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Color whiteColor = const Color(0XFFF2F2F2);
+  Color blackColor = const Color(0XFF0C110F);
+  Color buttonColor = const Color(0XFF00FFAE);
+
+  String qrData = 'https://github.com/mauriceprosper';
+  TextEditingController qrDataController = TextEditingController();
+  final qrKey = GlobalKey();
+
+  @override
+  void dispose() {
+    qrDataController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: blackColor,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(
+          IconlyLight.category,
+          color: whiteColor,
+        ),
+        actions: [
+          Icon(
+            IconlyLight.search,
+            color: whiteColor,
+          ),
+          const SizedBox(
+            width: 12,
+          ),
+          Icon(
+            IconlyLight.scan,
+            color: whiteColor,
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            Text(
+              'Create a personal QR-Code',
+              style: GoogleFonts.raleway(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: whiteColor,
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 400,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: RepaintBoundary(
+                key: qrKey,
+                child: QrImageView(
+                  data: qrData,
+                  version: QrVersions.auto,
+                  size: 300,
+                  dataModuleStyle: QrDataModuleStyle(
+                    dataModuleShape: QrDataModuleShape.circle,
+                    color: blackColor,
+                  ),
+                  eyeStyle: QrEyeStyle(
+                    eyeShape: QrEyeShape.square,
+                    color: blackColor,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              controller: qrDataController,
+              style: GoogleFonts.raleway(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: whiteColor,
+              ),
+              cursorColor: buttonColor,
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(
+                    color: buttonColor,
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  borderSide: BorderSide(
+                    color: buttonColor,
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(),
+            GestureDetector(
+              onTap: () {
+                if (qrDataController.text.isNotEmpty) {
+                  setState(() {
+                    qrData = qrDataController.text;
+                  });
+                }
+              },
+              child: Container(
+                height: 80,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: buttonColor,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: Center(
+                  child: Text(
+                    'Generate QR-Code',
+                    style: GoogleFonts.raleway(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: blackColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter QR Code Generator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0XFF00FFAE)),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
